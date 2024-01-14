39d690b6f3825f0da5bbe2b56da8cf912872cf96
taotao406 <ctlangtao@hotmail.com>
2024-01-14 09:35:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
 echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
