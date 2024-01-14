50502814405ab728a6b45ad51ddb219123c74220
taotao406 <ctlangtao@hotmail.com>
2024-01-14 09:59:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,5 @@
 
 # Add a feed source
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
-echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
-echo 'src-git freifunk https://github.com/freifunk/openwrt-packages.git' >>feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
